f060bcd509cd66c832b683e36ea9f4e66d202b01
111 <111>
2020-10-26 06:27:03 +0300

    App without a status bar

---
+++ Quizzler-iOS13/Questions.swift
@@ -0,0 +1,13 @@
+//
+//  Qestions.swift
+//  Quizzler-iOS13
+//
+//  Created by Lisistrata on 24.10.2020.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+struct Questions {
+    let text: String
+    let answer: String
+}
